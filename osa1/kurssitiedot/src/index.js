7ec518b0332f55545209de52a59364748c3c9e41
Janne Mikkelä <janne.mikkela@gmail.com>
2019-05-15 22:06:22 -0400

    Tehty 1.2

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -14,9 +14,17 @@ const App = () => {
     const Content = (props) => {
         return(
             <div>
-                <p>{props.part1} {props.exercises1}</p>
-                <p>{props.part2} {props.exercises2}</p>
-                <p>{props.part3} {props.exercises3}</p>
+                <Part part={props.part1} exercises={props.exercises1} />
+                <Part part={props.part2} exercises={props.exercises2} />
+                <Part part={props.part3} exercises={props.exercises3} />
+            </div>
+        )
+    }
+
+    const Part = (props) => {
+        return (
+            <div>
+                <p>{props.part} {props.exercises}</p>
             </div>
         )
     }
@@ -48,7 +56,6 @@ const App = () => {
       <Total exercises1={exercises1} exercises2={exercises2} 
       exercises3={exercises3} />
     </>
-
     )
   }
   
